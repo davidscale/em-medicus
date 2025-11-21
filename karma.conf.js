50b94c3ff43a6791af430d573006ad4b394b71cd
David <scale.davide@gmail.com>
2025-11-21 10:06:28 -0300

    subo ajustes para empezar a trabajar con medicus landing

--- karma.conf.js
+++ karma.conf.js
@@ -25,7 +25,7 @@ module.exports = function (config) {
       suppressAll: true // removes the duplicated traces
     },
     coverageReporter: {
-      dir: require('path').join(__dirname, './coverage/hominis'),
+      dir: require('path').join(__dirname, './coverage/medicus'),
       subdir: '.',
       reporters: [
         { type: 'html' },
